8bfd07102da38ff6f06c5166d5e1d7cdd5d8b355
alvaro.martin <alvaro.martin.fidalgo.contractor@bbva.com>
2022-07-29 15:32:57 +0200

    bug in transfers notice

--- biwenger/notices.py
+++ biwenger/notices.py
@@ -31,8 +31,9 @@ class TransfersNotice(Notice):
         prompted = []
         for day in data:
             for mov in day:
-                message = [mov["name"], "ficha por", str(mov["to"]["name"]), "por ", str(mov["amount"]) + " euros ",
-                           str(mov["amount"]-mov["price"]), "euros de diferencia sobre mercado"]
-                prompted.append(" ".join(message))
+                if "to" in mov.keys(): # TODO añadir from para ventas a mercado
+                    message = [mov["name"], "ficha por", str(mov["to"]["name"]), "por ", str(mov["amount"]) + " euros ",
+                               str(mov["amount"]-mov["price"]), "euros de diferencia sobre mercado"]
+                    prompted.append(" ".join(message))
         prompted.insert(0, self.template())
         return "\n".join(prompted)
