92010267605c48c1be924bc9eb542b39ff618482
alvaro.martin <alvaro.martin.fidalgo.contractor@bbva.com>
2022-08-05 10:23:10 +0200

    bug fixes, added player extended stats, html parsemode

--- biwenger/notices.py
+++ biwenger/notices.py
@@ -15,7 +15,7 @@ class MarketNotice(Notice):
     def show(self, data):
         prompted = []
         for log in data:
-            user = log['user'] if log['user'] is not None else 'Mercado'
+            user = log['user']['name'] if log['user'] is not None else 'Mercado'
             points_last = sum(filter(None, log['fitness']))
             message = [f'*{user}*', "vende a", f'*{log["name"]}*', "por", str(log["price"]), "euros.",
                        str(points_last), "puntos conseguidos en las últimas 5 jornadas que suman un total de",
@@ -38,14 +38,14 @@ class TransfersNotice(Notice):
             for mov in day:
                 if "to" in mov.keys():
                     message = [f'*{mov["name"]}*', "ficha por", str(mov["to"]["name"]), "por", str(mov["amount"]), "euros",
-                               str(mov["amount"]-mov["price"]), "euros de diferencia sobre mercado desde hoy. \n"]
+                               str(int((mov["amount"]-mov["price"])*100/mov['price'])), "% de diferencia sobre mercado desde hoy. \n"]
                     if "statusInfo" in mov.keys():
                         message.append(" ".join(["Duda por:", mov['statusInfo']]))
                     prompted.append(" ".join(message))
                 elif "from" in mov.keys():
                     message = [f'*{mov["from"]["name"]}*', "ha vendido a", f'*{mov["name"]}*', "a Mercado por",
-                               str(mov["amount"]), "euros,", str(mov["amount"]-mov["price"]),
-                               "euros de diferencia sobre mercado desde hoy. \n"]
+                               str(mov["amount"]), "euros,", str(int((mov["amount"]-mov["price"])*100/mov['price'])),
+                               "% de diferencia sobre mercado desde hoy. \n"]
                     if "statusInfo" in mov.keys():
                         message.append(" ".join([u'\U0001F915', "Duda por:", mov['statusInfo'], "\n"]))
                     prompted.append(" ".join(message))
--- telegram_client/app.py
+++ telegram_client/app.py
@@ -92,6 +92,10 @@ async def main() -> None:
     application = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
     # on different commands - answer in Telegram
     biwenger = BiwengerApi('alvarito174@hotmail.com', os.getenv("USER_PASS"))
+    # chat id private 855531130
+    # chat id group -1001673290336
+    #await application.bot.send_message(chat_id='855531130', text='<a href="#"><span class="tg-spoiler">spoiler</span> </a>',parse_mode='HTML')
+
     await application.bot.send_message(chat_id='-1001673290336', text=MarketNotice().show(biwenger.get_players_in_market()), parse_mode='Markdown')
     await application.bot.send_message(chat_id='-1001673290336', text=TransfersNotice().show(biwenger.get_last_user_transfers()), parse_mode='Markdown')
     # application.add_handler(CommandHandler("biwenger", init_biwenger_session))
