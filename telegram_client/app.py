2f475ce8277e6e67fcf20805d6a75fdb397e4b26
alvaro.martin <alvaro.martin.fidalgo.contractor@bbva.com>
2022-08-09 00:56:09 +0200

    group change

--- telegram_client/app.py
+++ telegram_client/app.py
@@ -96,11 +96,11 @@ async def main() -> None:
     group_chat = '-1001673290336'
     #await application.bot.send_message(chat_id='855531130', text='<a href="#"><span class="tg-spoiler">spoiler</span> </a>',parse_mode='HTML')
 
-    await application.bot.send_message(chat_id=private_chat, text=MarketNotice().
+    await application.bot.send_message(chat_id=group_chat, text=MarketNotice().
                                        show(biwenger.get_players_in_market()),
                                        disable_web_page_preview=True,
                                        parse_mode='Markdown')
-    await application.bot.send_message(chat_id=private_chat, text=TransfersNotice().
+    await application.bot.send_message(chat_id=group_chat, text=TransfersNotice().
                                        show(biwenger.get_last_user_transfers()),
                                        disable_web_page_preview=True,
                                        parse_mode='Markdown')
